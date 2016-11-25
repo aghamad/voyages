0fbd8090fa83ecb9cdfb4dd6d28b6bf53139b3bb
aghamad <201467122@collegeahuntsic.qc.ca>
2016-11-24 23:58:26 -0500

    Interface front-end pour qu'un administrateur puisse ajouter des voyages

---
+++ WebContent/ajout.jsp
@@ -0,0 +1,51 @@
+<%@ page language="java" contentType="text/html;charset=windows-1252" %>
+<%@ page import = "java.util.ArrayList" %>
+<%@ page import = "java.nio.file.Path" %>
+<%@ page import = "java.nio.file.Paths" %>
+<%@ page import = "java.io.InputStream" %>
+<%@ page import = "java.io.FileInputStream" %>
+<%@ page import = "java.io.File" %>
+<%@ page import = "java.util.Arrays" %>
+<%@ page import = "voyages.models.implementations.*" %>
+
+<%@ include file="head.jsp" %>
+
+ <div class="container">
+  <div class="row">
+     <div class="col-sm-6 col-md-4 col-md-offset-4">
+         <h1 class="text-center login-title">Ajouter un voyage</h1>
+         <div class="account-wall">
+             <img class="profile-img" src="https://cdn4.iconfinder.com/data/icons/aviation-cobalt/512/avion_aviation_wings_flying_airlines_avia_airliner-128.png" alt="">
+             
+	         <form class="form-signin" method=post>
+	             <input type="text" name=nom class="form-control" placeholder="Nom" required autofocus>
+	             <input type="text" name=prix class="form-control" placeholder="Prix" required autofocus>
+	             <textarea name="description" rows="4" cols="50" placeholder="Description" style="width:100%;"></textarea>
+	             
+	             <div class="input-group">
+		             <input type="text" id="datedebut" placeholder="Date début" style="width:100%;" required autofocus>
+		             <span class="input-group-addon">-></span>
+		             <input type="text" id="datefin" placeholder="Date fin" style="width:100%;" required autofocus>
+	             </div>
+	             
+	             <button class="btn btn-lg btn-primary btn-block" type="submit">
+	                 Ajouter voyage</button>
+	                       
+             </form>
+             
+         </div>
+     </div>
+ </div>
+ </div>
+
+<script>
+	$(function() {
+		$("#datedebut").datepicker();
+	});
+	  
+	$( function() {
+		$("#datefin").datepicker();
+	});
+</script>
+
+<%@ include file="foot.jsp" %>
---
+++ src/voyages/servlets/Ajout.java
@@ -0,0 +1,85 @@
+
+package voyages.servlets;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import exceptions.DAOException;
+import voyages.db.Connexion;
+import voyages.models.implementations.User;
+
+/**
+ * Servlet implementation class Ajout d'un voyage
+ */
+@WebServlet(
+    name = "Ajout",
+    urlPatterns = {"/ajout"})
+public class Ajout extends BaseServlet {
+    private static final long serialVersionUID = 1L;
+
+    private static final String CONTENT_TYPE = "text/html; charset=windows-1252";
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request,
+        HttpServletResponse response) throws ServletException,
+        IOException {
+        response.setContentType(CONTENT_TYPE);
+        // Add le voyage dans la bd
+        
+        // redirect to form escale if success
+        request.getRequestDispatcher("/ajoutEscale.jsp").forward(request,
+                response);
+        
+        /*
+        User new_user;
+
+        try {
+            new_user = new User(getConnexion());
+
+            new_user.Password = request.getParameter("Password");
+            new_user.FirstName = request.getParameter("FirstName");
+            new_user.LastName = request.getParameter("LastName");
+            new_user.Address = request.getParameter("Address");
+            new_user.Email = request.getParameter("Email");
+
+            new_user.create();
+            response.sendRedirect("/Commerce-Project1-context-root/login");
+
+        } catch(DAOException e) {
+            request.setAttribute("error",
+                e);
+        } catch(
+            ClassNotFoundException
+            | SQLException e) {
+            request.setAttribute("error",
+                e);
+        } catch(Exception e) {
+            request.setAttribute("error",
+                e);
+        }
+        request.getRequestDispatcher("/signup.jsp").forward(request,
+            response);
+       */
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request,
+        HttpServletResponse response) throws ServletException,
+        IOException {
+        response.setContentType(CONTENT_TYPE);
+
+        request.getRequestDispatcher("/ajout.jsp").forward(request,
+            response);
+    }
+}
